70f8e1cd39e8057708d34ce3b2b87403533cf4f7
tasnimfarhat27 <tasnimfarhat03@gmail.com>
2026-08-07 15:42:22 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,13 @@
+class array
+{
+	public static void main(String args[])
+	{
+		int [] arr ={14, 25, 34, 74, 91, 45, 76};
+		
+		for(int i = 0; i <arr.length; i++)
+		{
+
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
