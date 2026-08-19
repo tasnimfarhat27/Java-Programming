775c6830d66702d9b3c59e20d6aabb940d1c1856
tasnimfarhat27 <tasnimfarhat03@gmail.com>
2026-08-19 09:36:12 +0530

    Add files via upload

---
+++ constructor.java
@@ -0,0 +1,19 @@
+class A
+{
+	int a;
+	boolean b;
+	String c;
+}
+
+class B
+{
+	public static void main(String args[])
+	{
+		A obj = new A();
+		System.out.println(obj.a);
+		System.out.println(obj.b);
+		System.out.println(obj.c);
+
+		
+	}
+}
